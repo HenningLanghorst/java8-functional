7e9f731cdc30144a6d53589db479f021fa34a89a
Henning Langhorst <henninglanghorst@yahoo.de>
2016-05-16 23:27:12 +0200

    Code formatting

--- src/main/java/de/henninglanghorst/functional/sql/DatabaseUpdateFunctions.java
+++ src/main/java/de/henninglanghorst/functional/sql/DatabaseUpdateFunctions.java
@@ -9,7 +9,8 @@ import java.sql.SQLException;
 import java.util.Collection;
 
 /**
- * Class providing functions for updating database tables, usable with {@link DatabaseOperations#doInDatabase(Supplier, Function)}.
+ * Class providing functions for updating database tables, usable with
+ * {@link DatabaseOperations#doInDatabase(Supplier, Function)}.
  *
  * @author Henning Langhorst
  */
@@ -21,15 +22,16 @@ public final class DatabaseUpdateFunctions {
     /**
      * Returns a function which performs a SQL update on the database.
      *
-     * @param preparedStatementFactory Creates the update statement using a given {@link Connection}.
+     * @param psFactory Creates the update statement using a given {@link Connection}.
      * @return A Function performing the update and returning an {@link Integer} value.
      */
-    public static Function<Connection, Integer> databaseUpdate(final Function<Connection, PreparedStatement> preparedStatementFactory) {
-        return connection -> performUpdateOnConnection(connection, preparedStatementFactory);
+    public static Function<Connection, Integer> databaseUpdate(final Function<Connection, PreparedStatement> psFactory) {
+        return connection -> performUpdateOnConnection(connection, psFactory);
     }
 
     private static int performUpdateOnConnection(final Connection connection,
-                                                 final Function<Connection, PreparedStatement> preparedStatementFactory) throws SQLException {
+                                                 final Function<Connection, PreparedStatement> preparedStatementFactory)
+            throws SQLException {
         try (PreparedStatement preparedStatement = preparedStatementFactory.apply(connection)) {
             return preparedStatement.executeUpdate();
         }
@@ -38,17 +40,22 @@ public final class DatabaseUpdateFunctions {
     /**
      * Returns a function which performs multiple SQL update on the database.
      *
-     * @param preparedStatementFactories List of {@link PreparedStatement} factories creating update statements using a given {@link Connection}.
-     * @return A Function performing the updates and returning an {@code int[]} array value with the number of the updates rows per update.
+     * @param psFactories List of {@link PreparedStatement} factories creating update statements using a
+     *                                   given {@link Connection}.
+     * @return A Function performing the updates and returning an {@code int[]} array value with the number of the
+     * updates rows per update.
      */
-    public static Function<Connection, int[]> multipleDatabaseUpdates(final Collection<Function<Connection, PreparedStatement>> preparedStatementFactories) {
-        return connection -> performUpdatesOnConnection(connection, preparedStatementFactories);
+    public static Function<Connection, int[]> multipleDatabaseUpdates(final Collection<Function<Connection, PreparedStatement>> psFactories) {
+        return connection -> performUpdatesOnConnection(connection, psFactories);
     }
-    private static int[] performUpdatesOnConnection(final Connection connection, final Collection<Function<Connection, PreparedStatement>> preparedStatementFactories) throws SQLException {
-        int[] result = new int[preparedStatementFactories.size()];
+
+    private static int[] performUpdatesOnConnection(final Connection connection,
+                                                    final Collection<Function<Connection, PreparedStatement>> psFactories)
+            throws SQLException {
+        int[] result = new int[psFactories.size()];
         int currentIndex = 0;
-        for (Function<Connection, PreparedStatement> f : preparedStatementFactories) {
-            result[currentIndex++] = performUpdateOnConnection(connection, f);
+        for (Function<Connection, PreparedStatement> preparedStatementFactory : psFactories) {
+            result[currentIndex++] = performUpdateOnConnection(connection, preparedStatementFactory);
         }
         return result;
     }
