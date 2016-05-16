3c49fe115475a8b802b1e3fa4eea98f8fe8732c6
Henning Langhorst <henninglanghorst@yahoo.de>
2016-05-16 20:24:44 +0200

    Adding support for mutliple update statement execution and transaction handling

--- src/main/java/de/henninglanghorst/functional/Main.java
+++ src/main/java/de/henninglanghorst/functional/Main.java
@@ -6,9 +6,11 @@ import org.h2.jdbcx.JdbcConnectionPool;
 
 import java.sql.Date;
 import java.sql.SQLException;
+import java.util.Arrays;
 import java.util.List;
 
 import static de.henninglanghorst.functional.sql.DatabaseOperations.*;
+import static java.util.Arrays.asList;
 
 /**
  * Example program.
@@ -21,27 +23,28 @@ public class Main {
 
         final JdbcConnectionPool cp = JdbcConnectionPool.create("jdbc:h2:~/testdb", "sa", "");
 
-
         doInDatabase(cp::getConnection, databaseUpdate(statement("drop table Person")))
                 .handleResult(objects -> System.out.println("Success " + objects), Throwable::printStackTrace);
 
         doInDatabase(
                 cp::getConnection,
-                databaseUpdate(
-                        statement("create table Person (id integer primary key, firstName varchar2, lastName varchar2, birthday date)")))
+                withinTransaction(
+                        databaseUpdate(
+                                statement("create table Person (id integer primary key, firstName varchar2, lastName varchar2, birthday date)"))))
                 .handleResult(objects -> System.out.println("Success " + objects), Throwable::printStackTrace);
 
-        doInDatabase(
-                cp::getConnection,
-                databaseUpdate(
-                        statement("insert into Person values (?, ?, ?, ?);", 1, "Carl", "Carlsson", Date.valueOf("1972-04-02"))))
-                .handleResult(objects -> System.out.println("Success " + objects), Throwable::printStackTrace);
+        doInDatabase(cp::getConnection, multipleDatabaseUpdates(
+                asList(
+                        statement(
+                                "insert into Person values (?, ?, ?, ?);",
+                                1, "Carl", "Carlsson", Date.valueOf("1972-04-02")),
+                        statement("insert into Person values (?, ?, ?, ?);",
+                                2, "Lenny", "Leonard", Date.valueOf("1981-04-02")))
+                )
+        ).handleResult(
+                objects -> System.out.println("Inserted rows: " + Arrays.toString(objects)),
+                Throwable::printStackTrace);
 
-        doInDatabase(
-                cp::getConnection,
-                databaseUpdate(
-                        statement("insert into Person values (?, ?, ?, ?);", 2, "Lenny", "Leonard", Date.valueOf("1981-04-02"))))
-                .handleResult(objects -> System.out.println("Success " + objects), Throwable::printStackTrace);
 
 
         final Either<List<Person>, SQLException> result = doInDatabase(
@@ -52,8 +55,7 @@ public class Main {
                                 resultSet.getInt("id"),
                                 resultSet.getString("firstName"),
                                 resultSet.getString("lastName"),
-                                resultSet.getDate("birthday").toLocalDate())
-                ));
+                                resultSet.getDate("birthday").toLocalDate())));
 
 
         System.out.println(result.left().get());
